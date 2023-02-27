99632c969eabf579fab1171e6c4b0214d0d90c90
Jamie Taber-Plaisted <jamietp113@gmail.com>
2023-02-27 18:02:30 -0500

    intro finished

---
+++ MyFirstProgram/Main.java
@@ -0,0 +1,19 @@
+package MyFirstProgram;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("I love pizza");
+        System.out.println("It's really good!");
+
+        // \n acts as if a enter key was pressed
+        // \t acts as if a tab key was pressed
+        System.out.print("I love pizza\n");
+        System.out.print("\tIt's really good!");
+
+        // <-- this is a single line comment
+
+        /*
+         * <--- this is a multi-line comment
+         */
+    }
+}
